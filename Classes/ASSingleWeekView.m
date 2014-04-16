6ec18ba700ddca5048c85275f31513157acebaf8
Adrian Schoenig <adrian.schoenig@gmail.com>
2014-04-16 12:06:33 +1000

    Tapping on days

--- Classes/ASSingleWeekView.m
+++ Classes/ASSingleWeekView.m
@@ -22,21 +22,7 @@
 {
   _startDate = startDate;
   
-  // rebuild the view
-  for (NSInteger index = self.subviews.count - 1; index >= 0; index--) {
-    UIView *subview = self.subviews[index];
-    [subview removeFromSuperview];
-  }
-  CGFloat widthPerItem = CGRectGetWidth(self.frame) / 7;
-  CGFloat itemHeight = CGRectGetHeight(self.frame);
-  for (NSUInteger dayIndex = 0; dayIndex < 7; dayIndex++) {
-    NSDate *date = dayIndex == 0 ? startDate : [self dateByAddingDays:dayIndex toDate:startDate];
-    CGRect frame = CGRectMake(dayIndex * widthPerItem, 0, widthPerItem, itemHeight);
-    UIView *view = [self.delegate singleWeekView:self
-                                     viewForDate:date
-                                       withFrame:frame];
-    [self addSubview:view];
-  }
+  [self rebuildView];
 }
 
 - (id)initWithFrame:(CGRect)frame
@@ -44,17 +30,56 @@
   self = [super initWithFrame:frame];
   if (self) {
     self.gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    
+    UITapGestureRecognizer *tapper = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapper:)];
+    [self addGestureRecognizer:tapper];
   }
   return self;
+}
+
+#pragma mark - User interaction
+
+- (void)handleTapper:(UITapGestureRecognizer *)tapper
+{
+  CGPoint location = [tapper locationInView:self];
+  UIView *subview = [self hitTest:location withEvent:nil];
+  NSUInteger dayOffset = subview.tag;
+  
+  NSDate *date = [self dateByAddingDays:dayOffset toDate:self.startDate];
+  self.selectedDate = date;
+  [self.delegate singleWeekView:self didSelectDate:date];
 }
 
 #pragma mark - Private helpers
 
 - (NSDate *)dateByAddingDays:(NSInteger)days toDate:(NSDate *)date
 {
+  if (days == 0) {
+    return date;
+  }
+  
   NSDateComponents *components = [[NSDateComponents alloc] init];
   [components setDay:days];
   return [self.gregorian dateByAddingComponents:components toDate:date options:0];
+}
+
+- (void)rebuildView
+{
+  for (NSInteger index = self.subviews.count - 1; index >= 0; index--) {
+    UIView *subview = self.subviews[index];
+    [subview removeFromSuperview];
+  }
+  CGFloat widthPerItem = CGRectGetWidth(self.frame) / 7;
+  CGFloat itemHeight = CGRectGetHeight(self.frame);
+  for (NSUInteger dayIndex = 0; dayIndex < 7; dayIndex++) {
+    NSDate *date = [self dateByAddingDays:dayIndex toDate:self.startDate];
+    CGRect frame = CGRectMake(dayIndex * widthPerItem, 0, widthPerItem, itemHeight);
+    UIView *view = [self.delegate singleWeekView:self
+                                     viewForDate:date
+                                       withFrame:frame];
+    view.tag = dayIndex;
+    [self addSubview:view];
+  }
 }
 
 @end
